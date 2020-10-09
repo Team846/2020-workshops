d0e46a968448e3d4e080a6954bc8276137916982
Andrew (Andy) Min <me@andrewmin.info>
2020-10-08 21:46:05 -0700

    testing

--- _TEMPLATE.kts
+++ _TEMPLATE.kts
@@ -4,7 +4,7 @@ fun main() {
 
 // Normally the main function is automatically called,
 // but when doing Kotlin scripts (files that end with kts)
-// Has to be manually called.
+// has to be manually called.
 //
 // Just ignore
 main()
