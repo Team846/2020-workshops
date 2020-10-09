2b07a1562d313d69b814554875cb8606272a83ba
kaustubh <kaustubh.khulbe@gmail.com>
2020-10-08 18:45:29 -0700

    Template file test

---
+++ Kaustubh-Khulbe.kts
@@ -0,0 +1,11 @@
+fun main() {
+    // Your code here!
+    println("Dream is god")
+}
+
+// Normally the main function is automatically called,
+// but when doing Kotlin scripts (files that end with kts)
+// Has to be manually called.
+//
+// Just ignore
+main()
