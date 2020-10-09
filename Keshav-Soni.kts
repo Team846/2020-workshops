a1be1895b8a90c28397abdfa11f58ec0f6424ced
KeshavSoni17 <49103006+KeshavSoni17@users.noreply.github.com>
2020-10-08 21:06:46 -0700

    Add files via upload

---
+++ Keshav-Soni.kts
@@ -0,0 +1,10 @@
+fun main() {
+    print("Keshav Soni")
+}
+
+// Normally the main function is automatically called,
+// but when doing Kotlin scripts (files that end with kts)
+// Has to be manually called.
+//
+// Just ignore
+main()
---
+++ _TEMPLATE.kts
@@ -0,0 +1,10 @@
+fun main() {
+    // Your code here!
+}
+
+// Normally the main function is automatically called,
+// but when doing Kotlin scripts (files that end with kts)
+// Has to be manually called.
+//
+// Just ignore
+main()
